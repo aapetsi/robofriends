255b7c143164236ea00f5814c6927e791766469c
Apetsi <35830971+aapetsi@users.noreply.github.com>
2018-06-03 01:03:32 +0800

    first commit

---
+++ src/robots.js
@@ -0,0 +1,56 @@
+export const robots = [
+    {
+        id: 1,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    },
+    {
+        id: 2,
+        name: "sally bing",
+        username: "sally",
+        email: "sallybing@gmail.com"
+    },
+    {
+        id: 3,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    },
+    {
+        id: 4,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    },
+    {
+        id: 5,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    },
+    {
+        id: 6,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    },
+    {
+        id: 7,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    },
+    {
+        id: 8,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    },
+    {
+        id: 9,
+        name: "john doe",
+        username: "john",
+        email: "johndoe@gmail.com"
+    }
+]
